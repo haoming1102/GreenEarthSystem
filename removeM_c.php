2b14cb8bc430e414b9a6eee455910dce624d87b7
jing179 <40681800+jing179@users.noreply.github.com>
2020-03-04 21:36:16 +0800

    Create removeM_c.php

---
+++ removeM_c.php
@@ -0,0 +1,37 @@
+<?php
+    session_start();
+    $R_id = $_SESSION["usrid"];
+    $CMaterial_id = $_POST["collectormaterialID"];
+
+
+    //connect to database
+    $conn = new mysqli("localhost", "root", "", "greenearth");
+
+    // this is to find income by using id
+    $id= "SELECT * FROM collectormaterial WHERE COLLECTORMATERIAL_ID = '$CMaterial_id'";
+    $result = $conn->query($id);
+    $row = $result->fetch_assoc();
+
+
+
+    if($conn ->connect_error){
+      die("Connection error");
+    }
+
+
+    $sql = "DELETE FROM collectormaterial WHERE COLLECTORMATERIAL_ID = '$CMaterial_id'";
+
+    //execute the query
+    if($conn->query($sql) == TRUE){
+        echo "<script type='text/javascript'> alert('Remove successfully'); </script>";
+        echo "<script type='text/javascript'> window.location='viewMaterial_c.php'</script>";
+
+
+    }
+    else{
+      echo "Fail";
+    }
+
+
+
+?>
