7fbd0faed5f7fea37485f11e973a3d8e2911cb50
haoming1102 <40682161+haoming1102@users.noreply.github.com>
2020-03-14 17:25:59 +0800

    Update updateMaterial.php
    Bug fix from iteration 1

--- assets/php/updateMaterial.php
+++ assets/php/updateMaterial.php
@@ -1,14 +1,15 @@
 <?php
-//get the data from manageMaterial.php
-$materialID = $_POST["materialID"];
-$materialName = $_POST["materialName"];
-$pointsPerKg = $_POST["pointsPerKg"];
-$description = $_POST["description"];
-
 $conn = new mysqli("localhost","root","", "greenearth");
 if ($conn->connect_error){
 	die("Connection failure");
 }
+
+//get the data from manageMaterial.php
+//prevent database error due to user's input
+$materialID = $_POST["materialID"];
+$materialName = mysqli_real_escape_string($conn, $_POST["materialName"]);
+$description = mysqli_real_escape_string($conn, $_POST["description"]);
+$pointsPerKg = mysqli_real_escape_string($conn, $_POST["pointsPerKg"]);
 
 //update the material data
 $updateData = "update material set MATERIAL_NAME='$materialName', POINTSPERKG='$pointsPerKg', DESCRIPTION='$description' where MATERIAL_ID='$materialID';";
